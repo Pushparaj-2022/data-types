e83293e98a6cc4afd1d39a4f179334641c0a6b1f
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-02 14:30:10 +0530

    vartype

--- vartype.java
+++ vartype.java
@@ -9,8 +9,8 @@ public class vartype {
         var option = 'A';
         var friend = false;
 
-        System.out.println("I have given my friend " + x + " and he borrowed my another friend " + y
-                + " and promised to give us total. " + n + " but his friend give only " + paid
+        System.out.println("I have given my friend " + name + " " + x + " and he borrowed my another friend " + y
+                + " and promised to give us total " + n + " but his friend give only " + paid
                 + "and said to my friend he paid. but that's not true he " + friend
                 + "ly lied aslo he did not paid what he promised and his another friend not get signle penny he paid. ");
 
