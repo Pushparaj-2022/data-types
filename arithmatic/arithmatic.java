ee7313652737a15e49fe4129ce9443a5c1141b6b
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-05 18:36:06 +0530

    first update

---
+++ arithmatic/arithmatic.java
@@ -0,0 +1,19 @@
+public class arithmatic {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 5;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        int inc = 5;
+
+        ++inc;
+        System.out.println(inc);
+        // --inc;
+        // System.out.println(inc);
+
+    }
+}
