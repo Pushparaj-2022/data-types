7a902b4f5bd068ce16600f336d53c2a843c0c74b
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-08 14:43:46 +0530

    update

--- assignment/assignment.java
+++ assignment/assignment.java
@@ -5,12 +5,16 @@ public class assignment {
 // System.out.println(v1);
 
 
-int v2 =5;
-v2 -= 5;
-System.out.println(v2);
-    }
+// int v2 =5;
+// v2 -= 5;
+// System.out.println(v2);
+
+int v3 = 10;
+v3 *= 5;
+System.out.println(v3); 
 
 
 
-    
+
+   }
 }
