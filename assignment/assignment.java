d531b07618939c4e8ea2227a1e51ff52fcb935e2
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-08 14:41:31 +0530

    update

--- assignment/assignment.java
+++ assignment/assignment.java
@@ -1,9 +1,16 @@
 public class assignment {
     public static void main(String[] args) {
-        int v1 = 5;
-        v1 += 5;
-System.out.println(v1);
+//         int v1 = 5;
+//         v1 += 5;
+// System.out.println(v1);
 
 
+int v2 =5;
+v2 -= 5;
+System.out.println(v2);
     }
+
+
+
+    
 }
