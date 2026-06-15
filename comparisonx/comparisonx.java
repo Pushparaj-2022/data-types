010d175c63c9e0e2a2c06af99e3721730e6a0e0a
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-15 16:08:10 +0530

    update

--- comparisonx/comparisonx.java
+++ comparisonx/comparisonx.java
@@ -7,9 +7,21 @@ public class comparisonx {
         // int b = 5;
         // System.out.println(a == b);
 
-        int a = 5;
-        int b = 4;
-        System.out.println(a == b);
+        // int a = 5;
+        // int b = 4;
+        // System.out.println(a == b);
 
+        
+        // int a = 5;
+        // int b = 5;
+        // System.out.println(a != b);
+
+        // int a = 15;
+        // int b = 10;
+        // System.out.println(a >= b);
+
+        int x = 20;
+        int y = 25;
+        System.out.println(x >= y);
     }
 }
