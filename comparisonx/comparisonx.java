95be15dbe915f28a19137c57f9f84b2a8da658b1
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-15 18:03:16 +0530

    update

--- comparisonx/comparisonx.java
+++ comparisonx/comparisonx.java
@@ -11,7 +11,6 @@ public class comparisonx {
         // int b = 4;
         // System.out.println(a == b);
 
-        
         // int a = 5;
         // int b = 5;
         // System.out.println(a != b);
@@ -20,8 +19,13 @@ public class comparisonx {
         // int b = 10;
         // System.out.println(a >= b);
 
-        int x = 20;
-        int y = 25;
-        System.out.println(x >= y);
+        int x = 10;
+        int y =20;
+        System.out.println(x > y);
+
+        // int age = 18;
+        // System.out.println(age < 18);
+        // System.out.println(age > 18);
+
     }
 }
