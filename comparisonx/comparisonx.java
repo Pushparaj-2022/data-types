444c0bf41007041980a8e3280081521c9044718c
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-15 14:32:42 +0530

    update

---
+++ comparisonx/comparisonx.java
@@ -0,0 +1,15 @@
+package comparisonx;
+
+public class comparisonx {
+    public static void main(String[] args) {
+
+        // int a = 5;
+        // int b = 5;
+        // System.out.println(a == b);
+
+        int a = 5;
+        int b = 4;
+        System.out.println(a == b);
+
+    }
+}
