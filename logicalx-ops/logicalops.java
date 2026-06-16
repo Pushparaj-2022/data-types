5317d21395ad42054f68c54aa3085552ace574ae
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-16 13:22:33 +0530

    update

--- logicalx-ops/logicalops.java
+++ logicalx-ops/logicalops.java
@@ -9,6 +9,15 @@ public class logicalops {
         // int x = 10;
 
         // System.out.println(!(x >20 && x< 0));
+
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+
+        System.out.println("is user logged in:"+ (isLoggedIn && !isAdmin));
+
+        System.out.println("The User Has Acces:"+ (isLoggedIn || isAdmin));
+        System.out.println("not logged in:"+(!isLoggedIn));
+
     }
 
 }
