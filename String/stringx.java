8e8bcf1505ed4e2fc1cc890a0c5c3c8aec63de23
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-16 16:38:44 +0530

    update

---
+++ String/stringx.java
@@ -0,0 +1,12 @@
+public class stringx{
+    public static void main(String[] args){
+
+// String letters = "asdanfkjdand";
+// System.out.println(letters.length());
+
+
+String upper= "pattasu balu";
+System.out.println(upper.toUpperCase());
+
+    }
+}
