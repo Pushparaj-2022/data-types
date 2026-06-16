e56e86d6699bbe89e907ff231854c11aa67ac1ec
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-16 13:23:43 +0530

    update

--- logicalx-ops/logicalops.java
+++ logicalx-ops/logicalops.java
@@ -2,21 +2,21 @@ public class logicalops {
 
     public static void main(String[] args) {
 
-        // int x = 10;
+        int x = 10;
 
-        // System.out.println(x > 20 | x < 0);
+        System.out.println(x > 20 | x < 0);
 
-        // int x = 10;
+        int x = 10;
 
-        // System.out.println(!(x >20 && x< 0));
+        System.out.println(!(x >20 && x< 0));
 
-        // boolean isLoggedIn = true;
-        // boolean isAdmin = false;
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
 
-        // System.out.println("is user logged in:"+ (isLoggedIn && !isAdmin));
+        System.out.println("is user logged in:"+ (isLoggedIn && !isAdmin));
 
-        // System.out.println("The User Has Acces:"+ (isLoggedIn || isAdmin));
-        // System.out.println("not logged in:"+(!isLoggedIn));
+        System.out.println("The User Has Acces:"+ (isLoggedIn || isAdmin));
+        System.out.println("not logged in:"+(!isLoggedIn));
 
     }
 
