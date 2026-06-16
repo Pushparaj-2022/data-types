20d3bd0c1a4f11fa20a49fd69bed54aa61a983c1
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-16 18:06:41 +0530

    update

--- String/stringx.java
+++ String/stringx.java
@@ -1,21 +1,20 @@
-public class stringx{
-    public static void main(String[] args){
+public class stringx {
+    public static void main(String[] args) {
 
-// String letters = "asdanfkjdand";
-// System.out.println(letters.length());
+        // String letters = "asdanfkjdand";
+        // System.out.println(letters.length());
 
+        // String upper= "pattasu balu";
+        // System.out.println(upper.toUpperCase());
 
-// String upper= "pattasu balu";
-// System.out.println(upper.toUpperCase());
+        // String upper= "BOMB BALU";
+        // System.out.println(upper.toLowerCase());
 
+        // String upper= "Java counts positions from zero";
+        // System.out.println(upper.indexOf("z"));
 
-// String upper= "BOMB BALU";
-// System.out.println(upper.toLowerCase());
-
-
-String upper= "Java counts positions from zero";
-System.out.println(upper.indexOf("z"));
-
+        String upper = "Helo world";
+        System.out.println(upper.charAt(1));
 
     }
 }
