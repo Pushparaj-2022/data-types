a65924d3cde288543632963c2a163a94f48eef6f
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-17 12:36:28 +0530

    update

--- math/mathx.java
+++ math/mathx.java
@@ -3,13 +3,17 @@ package math;
 public class mathx {
     public static void main(String[] args) {
 
-        System.out.println(Math.max(20, 50));
+        // System.out.println(Math.max(20, 50));
 
-        System.out.println(Math.min(20, 0));
+        // System.out.println(Math.min(20, 0));
 
-        System.out.println(Math.sqrt(20));
+        // System.out.println(Math.sqrt(20));
 
-        System.out.println(Math.abs(-20.7));
+        // System.out.println(Math.abs(-20.7));
+
+        System.out.println(Math.pow(2, 10));
+
+
 
     }
 }
