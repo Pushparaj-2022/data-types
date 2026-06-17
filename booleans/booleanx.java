8f8097fdec13d19bf2a78a5b8b2e394783c10b4a
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-17 17:39:27 +0530

    update

---
+++ booleans/booleanx.java
@@ -0,0 +1,9 @@
+public class booleanx {
+    public static void main(String[] args) {
+
+        boolean isJavaFun = true;
+        boolean javaIsHard = false;
+        System.out.println(isJavaFun || javaIsHard);
+
+    }
+}
