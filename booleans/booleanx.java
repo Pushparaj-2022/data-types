179c6da423bac77d58ba33165db6e259650d434d
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-17 17:57:52 +0530

    update

--- booleans/booleanx.java
+++ booleans/booleanx.java
@@ -1,9 +1,26 @@
 public class booleanx {
     public static void main(String[] args) {
 
-        boolean isJavaFun = true;
-        boolean javaIsHard = false;
-        System.out.println(isJavaFun || javaIsHard);
+        // boolean isJavaFun = true;
+        // boolean javaIsHard = false;
+        // System.out.println(isJavaFun || javaIsHard);
+
+        // int x = 10;
+        // int y = 5;
+        // System.out.println(x > y );
+        // System.out.println(x < y && x == 10 );
+
+        // // Store the value in boolen variable
+        // int x = 10;
+        // int y = 10;
+        // boolean xy = x >= y;
+        // System.out.println(xy);
+
+
+        int myage = 20;
+        int votingAge =18;
+        boolean voting = myage >= votingAge;
+        System.out.println(voting);
 
     }
 }
