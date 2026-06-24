4137260595427dfcc72a15d8cdbcf83edab988d0
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-24 11:21:41 +0530

    Update code

--- switchx/switchx.java
+++ switchx/switchx.java
@@ -1,6 +1,6 @@
 public class switchx {
     public static void main(String[] args) {
-        int day = 6;
+        int day = 5;
         switch (day) {
             case 1:
                 System.out.println("Monday");
