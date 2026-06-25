0128bc6398ee65ce24aa023a2429076ecdb830f1
Pushparaj-2022 <vanmakutty@gmail.com>
2026-06-25 17:47:12 +0530

    Update code

--- loops/loops.java
+++ loops/loops.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class loops {
     public static void main(String[] args) {
-        int i = 0;
+        // int i = 0;
 
-        while (i < 5) {
-            System.out.println(i);
-            i++;
+        // while (i < 15) {
+        // System.out.println(i);
+        // i++;
+        // }
+        int choice = 2;
+        Scanner scanner = new Scanner(System.in);
+
+        while (choice != 4) {
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+
+            choice = scanner.nextInt();
+
         }
+
     }
 }
