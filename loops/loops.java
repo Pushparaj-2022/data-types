8cd6d44b8d52939eba4e3b3d2298398cfa1e3d00
Pushparaj-2022 <vanmakutty@gmail.com>
2026-07-02 14:40:43 +0530

    update

--- loops/loops.java
+++ loops/loops.java
@@ -29,9 +29,9 @@ public class loops {
         //     i++;
         // } while (i <= 5);
 
-       for (int i =0; i <= 5 ; i++){
-        System.out.println(i);
-       }
+    //    for (int i =0; i <= 5 ; i= i + 2){
+    //     System.out.println(i);
+    //    }
 
      
 
