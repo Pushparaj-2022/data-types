a08c9f7069b812850ac1ed1a60534d76b2df351c
Pushparaj-2022 <vanmakutty@gmail.com>
2026-07-18 15:19:25 +0530

    bug fixing time, candle issue, scanner issue

--- array/array.java
+++ array/array.java
@@ -4,8 +4,9 @@ public class array {
     public static void main(String[] args){
 
 
-String[] cars ={ "volvo, bmv, ford, skoda, audi, Tata, Mahindra"};
+String[] cars ={ "volvo", "bmv", "ford", "skoda", "audi", "Tata", "Mahindra"};
+cars[0] ="Toyota";
+System.out.println(cars[1]);
 
-System.out.println(cars[0]);
     }
 }
