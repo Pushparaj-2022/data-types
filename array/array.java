666c0e743b40ba6f2ae186d8f5164ec3aa4f0040
Pushparaj-2022 <vanmakutty@gmail.com>
2026-07-18 11:47:15 +0530

    array

---
+++ array/array.java
@@ -0,0 +1,11 @@
+package array;
+
+public class array {
+    public static void main(String[] args){
+
+
+String[] cars ={ "volvo, bmv, ford, skoda, audi, Tata, Mahindra"};
+
+System.out.println(cars[0]);
+    }
+}
