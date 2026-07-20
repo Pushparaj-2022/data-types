2e6deb8f5f47f8b5622d1dde74833f972070dafc
Pushparaj-2022 <vanmakutty@gmail.com>
2026-07-20 12:11:09 +0530

    array

--- array/array.java
+++ array/array.java
@@ -1,13 +1,19 @@
 package array;
 
 public class array {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        // String[] cars ={ "volvo", "bmv", "ford", "skoda", "audi", "Tata",
+        // "Mahindra"};
+        // cars[0] ="Toyota";
+        // System.out.println(cars.length);
+        // // System.out.println(cars[1]);
 
-String[] cars ={ "volvo", "bmv", "ford", "skoda", "audi", "Tata", "Mahindra"};
-cars[0] ="Toyota";
-System.out.println(cars.length);
-// System.out.println(cars[1]);
+        String[] cars = { "volvo", "bmv", "ford", "skoda", "audi", "Tata", "Mahindra" };
+
+        for (int x = 0; x < cars.length; x++) {
+            System.out.println(cars[x]);
+        }
 
     }
 }
