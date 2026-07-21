042139b8aecf867785a8447e543dc943a15deeae
Pushparaj-2022 <vanmakutty@gmail.com>
2026-07-21 16:58:26 +0530

    array

--- array/array.java
+++ array/array.java
@@ -9,11 +9,21 @@ public class array {
         // System.out.println(cars.length);
         // // System.out.println(cars[1]);
 
-        // String[] cars = { "volvo", "bmv", "ford", "skoda", "audi", "Tata", "Mahindra" };
+        // String[] cars = { "volvo", "bmv", "ford", "skoda", "audi", "Tata", "Mahindra"
+        // };
 
         // for (int x = 0; x < cars.length; x++) {
-        //     System.out.println(cars[x]);
+        // System.out.println(cars[x]);
         // }
+
+        int[] num = { 1, 5, 10, 20, 30 };
+        int sum = 0;
+        for (int x = 0; x < num.length; x++) {
+            sum += num[x];
+
+        }
+
+        System.out.println(sum);
 
     }
 }
