60c564d71262afd9a8f0b6b77bfe794b4efda073
Pushparaj-2022 <vanmakutty@gmail.com>
2026-08-05 16:39:53 +0530

    Java methods

---
+++ methods/methods.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class methods {
+    static void hw(){
+        System.out.println("Hello Wolrd");
+    }
+
+    public static void main(String[] args){
+        hw();
+        hw();
+        hw();
+    }
+}
