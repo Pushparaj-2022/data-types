77985baa91f9bf8af060f328ec7ab4e251ea144b
Pushparaj-2022 <vanmakutty@gmail.com>
2026-08-10 15:06:15 +0530

    methods with return values

--- methods/methods.java
+++ methods/methods.java
@@ -34,20 +34,31 @@ public class methods {
     // family("parava ", 28);
     // }
 
-
     // methods with if else statement.....................
-    static void checkAge(int age) {
-        if (age < 18) {
-            System.out.println("Access Denied You're not authorized");
-        } else {
-            System.out.println("Access Granted");
-        }
+    // static void checkAge(int age) {
+    // if (age < 18) {
+    // System.out.println("Access Denied You're not authorized");
+    // } else {
+    // System.out.println("Access Granted");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // checkAge(20);
+    // checkAge(17);
+    // }
+
+    // return value .....................
+
+    static int add(int x) {
+        return x + 5;
+
     }
 
     public static void main(String[] args) {
-        checkAge(20);
-        checkAge(17);
+        System.out.println(add(5));
     }
 
 
+    
 }
