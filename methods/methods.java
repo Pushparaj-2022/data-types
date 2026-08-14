b8a9d9ecee3e742ef730ed0bb4e4e21439a8352e
Pushparaj-2022 <vanmakutty@gmail.com>
2026-08-14 12:19:03 +0530

    java recursion

--- methods/methods.java
+++ methods/methods.java
@@ -103,17 +103,20 @@ public class methods {
     // java recursion method
 
 
-    public static int sum(int x) {
-        if (x > 0) {
-            return x + sum(x - 1);
-        } else {
-            return 0;
-        }
-    }
+    // public static int sum(int x) {
+    //     if (x > 0) {
+    //         return x + sum(x - 1);
+    //     } else {
+    //         return 0;
+    //     }
+    // }
 
-    public static void main(String[] args) {
-        int result = sum(10);
-        System.out.println(result);
-    }
+    // public static void main(String[] args) {
+    //     int result = sum(10);
+    //     System.out.println(result);
+    // }
 
+
+
+    
 }
