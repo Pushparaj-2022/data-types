e8b90ba51904a9a25db694c77017df3889779e9a
Pushparaj-2022 <vanmakutty@gmail.com>
2026-08-25 12:08:15 +0530

    Java attributes

--- objects/Objects.java
+++ objects/Objects.java
@@ -1,21 +1,28 @@
- 
+
 // public class Objects {
 
-    // java object basic
-public class Objects{
-   public int x = 5;
-}
-    // public static void main(String[] args) {
-    //     Objects Ob = new Objects();
-    //     Objects Obx = new Objects();
-    //     System.out.println(Ob.x);
-    //     System.out.println(Obx.x);
-
-    // }
-
-
-
-
-    
+// java object basic
+// public static void main(String[] args) {
+// Objects Ob = new Objects();
+// Objects Obx = new Objects();
+// System.out.println(Ob.x);
+// System.out.println(Obx.x);
 
 // }
+
+// }
+
+// java attributes............
+
+public class Objects {
+    int x = 5;
+    int y = 10;
+
+    public static void main(String[] args) {
+        Objects obj = new Objects();
+        System.out.println(obj.x);
+        System.out.println(obj.y);
+
+    }
+
+}
