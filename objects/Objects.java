3c5b9b5022730064308ff99fd12a48f4205b1ad1
Pushparaj-2022 <vanmakutty@gmail.com>
2026-08-25 14:31:01 +0530

    Java multiple attributes

--- objects/Objects.java
+++ objects/Objects.java
@@ -15,16 +15,29 @@
 // java attributes............
 
 public class Objects {
-    int x = 5;
-    // int y = 10;
+    // int x = 5;
+    // // int y = 10;
+
+    // public static void main(String[] args) {
+    // Objects obj = new Objects();
+    // Objects obj1 = new Objects();
+    // obj1.x = 40;
+    // System.out.println(obj.x);
+    // System.out.println(obj1.x);
+
+    // }
+
+
+
+    // multiple attributes.................
+
+    String fname = "Bomb ";
+    String lname = "Pakkiri ";
+    int Age = 45;
 
     public static void main(String[] args) {
-        Objects obj = new Objects();
-        Objects obj1 = new Objects();
-        obj1.x = 40;
-        System.out.println(obj.x);
-        System.out.println(obj1.x);
-
+        Objects obx = new Objects();
+        System.out.println("The MC name is " + obx.fname + obx.lname + "and his age is " + obx.Age);
     }
 
 }
